57e94ff0307807a0bde3785c569c65e49133fd8e
Dan Gorman <danielhgorman@gmail.com>
2016-08-12 11:07:20 -0400

    working associations

--- final_project/app/models/user.rb
+++ final_project/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+	has_many :user_interests
+	has_many :user_organizations
+	has_many :organizations, through: :user_organizations
+	has_many :interests, through: :user_interests
+	
+
 end
