39211dda95660a145c94125e3c7943fc0703fb0b
ToddRizley <todd.rizley@gmail.com>
2016-08-13 15:31:09 -0400

    move factories to spec folder

---
+++ final_project/test/factories/jobs.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :job do
+    title "Savior"
+    description "Plz stop the Emperor and just save us. Money v good."
+    url "www.stoppalpatine.com"
+    # user_id
+    #location_id
+  end
+
+end
