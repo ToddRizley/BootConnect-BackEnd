90aa513165045e9382ad2a4a27b0a68edb24895f
ToddRizley <todd.rizley@gmail.com>
2016-08-13 16:12:17 -0400

    set up seed data, no associations yet

--- final_project/db/seeds.rb
+++ final_project/db/seeds.rb
@@ -1,67 +1,78 @@
 require 'factory_girl_rails'
 require 'faker'
 
+User.destroy_all
+Article.destroy_all
+Admin.destroy_all
+Location.destroy_all
+Organization.destroy_all
+Interest.destroy_all
+Job.destroy_all
 
+##add associations to models!!!
 
-#users
-FactoryGirl.define do
-  factory :user do
-    name Faker::StarWars.character ##Name.name
-    user_name Faker::Internet.user_name
-    position Faker::Name.title
-    company Faker::Company.name
-    bio Faker::Hipster.paragraph
-    email_address Faker::Internet.email
+# create organizations
+10.times do
+  FactoryGirl.create :organization, name: Faker::University.name
+end
 
+#create locations
+10.times do
+  FactoryGirl.create(:location,
+    name: Faker::Address.street_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    zipcode: Faker::Address.zip_code
+    )
   end
 end
 
-# t.integer  "organization_id" Do org associations!! for user and admin
-
-FactoryGirl.define do
-  factory :admin do
-    name Faker::Name.name
+#create users
+10.times do
+  FactoryGirl.create(:user,
+    name: Faker::StarWars.character,
+    user_name: Faker::Internet.user_name,
+    position: Faker::Name.title,
+    company: Faker::Company.name,
+    bio: Faker::Hipster.paragraph,
+    email_address: Faker::Internet.email
+    )
   end
 end
 
-FactoryGirl.define do
-  factory :article do
-    title Faker::Book.title
-    url Faker::Internet.url
-    ##user_id
-    ##org_id
+#create admins
+5.times do
+  FactoryGirl.create(:admin,
+    name: Faker::Name.name
+    )
   end
 end
 
-FactoryGirl.define do
-  factory :interest do
-    name Faker::
-    description Faker::Hipster.paragraph
+#create articles
+20.times do
+  FactoryGirl.create(:article,
+    title: Faker::Book.title,
+    url: Faker::Internet.url
+    )
   end
 end
 
-FactoryGirl.define do
-  factory :job do
-    title Faker::Name.title
-    description Faker::Hipster.sentence
-    url Faker::Internet.url
-    # user_id
-    #location_id
-  end
-
-end
-
-FactoryGirl.define do
-  factory :location do
-    name Faker::Address.street_address
-    city Faker::Address.city
-    state Faker::Address.state
-    zipcode Faker::Address.zip_code
+#create interests
+15.times do
+  FactoryGirl.create(:interest,
+      name: Faker::Hipster.word,
+      description: Faker::Hipster.paragraph
+      )
   end
 end
 
-FactoryGirl.define do
-  factory :organization do
-    name Faker::University.name
+#create jobs
+
+20.times do
+  FactoryGirl.create(:job,
+    title: Faker::Name.title,
+    description: Faker::Hipster.sentence,
+    url: Faker::Internet.url
+    )
   end
 end
