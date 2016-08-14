fa676e26e92dfd2528b85fc1152e0f8ac558d6e9
Jonathan Alexander <jonathandalexander@gmail.com>
2016-08-14 16:20:56 -0400

    remove validations for admin that were causing issues

--- final_project/app/models/admin.rb
+++ final_project/app/models/admin.rb
@@ -2,10 +2,10 @@ class Admin < ApplicationRecord
   belongs_to :organization, optional: true
 
   #name, email, password, confirm password
-  validates :name, presence: true
-
-  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  validates_format_of :email_address, with: EMAIL_REGEX, :on => :create
-  validates :email_address, uniqueness: true
-  #has_secure_password
+  # validates :name, presence: true
+  #
+  # EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  # validates_format_of :email_address, with: EMAIL_REGEX, :on => :create
+  # validates :email_address, uniqueness: true
+  # #has_secure_password
 end
