d59e043f0c7c9cadb060e177fe5a67c41971b2fb
ToddRizley <todd.rizley@gmail.com>
2016-08-25 11:55:25 -0400

    add company attribute to jobs serializer

--- final_project/app/serializers/job_serializer.rb
+++ final_project/app/serializers/job_serializer.rb
@@ -1,5 +1,5 @@
 class JobSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :url, :location
+  attributes :id, :title, :description, :url, :location, :company
   belongs_to :user
   belongs_to :location
 end
