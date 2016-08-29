405c0ad34eed66db46c1ca4f2cd85bf6b076f5c0
ToddRizley <todd.rizley@gmail.com>
2016-08-29 11:20:33 -0400

    delete file

--- final_project_react/frontend_react/src/reducers/myCurrentUser.js
+++
@@ -1,9 +0,0 @@
-export default function setCurrentUser(state = {current_user: []}, action){
-
-  switch(action.type){
-    case 'SET_CURRENT_USER':
-      return {current_user: action.payload.data };
-    default:
-      return state
-  }
-}
